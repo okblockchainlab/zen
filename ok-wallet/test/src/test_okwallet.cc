2123909f26f01bfac9670b472c30b20a5ae0dce6
fatcat22 <pandayt@163.com>
2018-07-31 18:10:09 +0800

    test success

--- ok-wallet/test/src/test_okwallet.cc
+++ ok-wallet/test/src/test_okwallet.cc
@@ -59,23 +59,22 @@ public:
 void* OKWalletTest::wlt_mod = NULL;
 execute_t OKWalletTest::execute = nullptr;
 
-const char* OKWalletTest::strSecret = "";
-const char* OKWalletTest::addr = "";
-const char* OKWalletTest::strSecretC = "";
-const char* OKWalletTest::addrC = "";
+const char* OKWalletTest::strSecret = "5HxWvvfubhXpYYpS3tJkw6fq9jE9j18THftkZjHHfmFiWtmAbrj";
+const char* OKWalletTest::addr = "znpLkpn6pif56eVZm9t85DWtwwEF8Rx2DHU";
+const char* OKWalletTest::strSecretC = "Kwr371tjA9u2rFSMZjTNun2PXXP3WPZu2afRHTcta6KxEUdm1vEw";
+const char* OKWalletTest::addrC = "znntDrEWVbxgwGJZS9qgZBter22KuXw5Cda";
 
-/*
 TEST_F(OKWalletTest, getaddressbyprivatekey) {
   std::list<std::string> result;
 
   auto cmd = std::string("getaddressbyprivatekey ") + strSecret;
-  execute("testnet", cmd, result);
+  execute("main", cmd, result);
 
   ASSERT_EQ((size_t)2, result.size());
   ASSERT_STREQ(addr, result.back().c_str());
 
   cmd = std::string("getaddressbyprivatekey ") + strSecretC;
-  execute("testnet", cmd, result);
+  execute("main", cmd, result);
 
   ASSERT_EQ((size_t)2, result.size());
   ASSERT_STREQ(addrC, result.back().c_str());
@@ -88,18 +87,18 @@ TEST_F(OKWalletTest, createrawtransaction) {
         std::string("{\"") + addrC + std::string("\":11}");
 
   std::list<std::string> result;
-  execute("testnet", cmd, result);
+  execute("main", cmd, result);
 
   ASSERT_EQ((size_t)2, result.size());
   ASSERT_STREQ(
-    "0c00000043497fd7f826957108f4a30fd9cec3aeba79972084e90ead01ea33090000000001f393847c97508f24b772281deea475cd3e0f719f321794e5da7cf8587e28ccb40100000000ffffffff0180778e06000000001976a914e20a55c7930322b0f730365618c7e59b9c99959d88ac00000000",
+    "0100000001f393847c97508f24b772281deea475cd3e0f719f321794e5da7cf8587e28ccb40100000000ffffffff0100ab9041000000003c76a914ef1caef64ee1e2aff877b7e8e9713dc25b2289f288ac200206260143838b5ff52dc2eb7b4b8099d4e4c99dc3ef19794289a2cd4c10070000b400000000",
     result.back().c_str()
   );
 }
 
 TEST_F(OKWalletTest, signrawtransaction) {
   const auto& cmd = "signrawtransaction "
-                    "0200000001f393847c97508f24b772281deea475cd3e0f719f321794e5da7cf8587e28ccb40100000000ffffffff0100ab90410000000017a914b10c9df5f7edf436c697f02f1efdba4cf39961518700000000 "
+                    "0100000001f393847c97508f24b772281deea475cd3e0f719f321794e5da7cf8587e28ccb40100000000ffffffff0100ab9041000000003c76a914ef1caef64ee1e2aff877b7e8e9713dc25b2289f288ac200206260143838b5ff52dc2eb7b4b8099d4e4c99dc3ef19794289a2cd4c10070000b400000000 "
                     "[{\"txid\":\"b4cc287e58f87cdae59417329f710f3ecd75a4ee1d2872b7248f50977c8493f3\",\"vout\":1,\"scriptPubKey\":\"a914b10c9df5f7edf436c697f02f1efdba4cf399615187\""
                     ",\"redeemScript\":\"512103debedc17b3df2badbcdd86d5feb4562b86fe182e5998abd8bcd4f122c6155b1b21027e940bb73ab8732bfdf7f9216ecefca5b94d6df834e77e108f68e66f126044c052ae\"}]"
                     " [\"KzsXybp9jX64P5ekX1KUxRQ79Jht9uzW7LorgwE65i5rWACL6LQe\",\"Kyhdf5LuKTRx4ge69ybABsiUAWjVRK4XGxAKk2FQLp2HjGMy87Z4\"]";
@@ -111,11 +110,10 @@ TEST_F(OKWalletTest, signrawtransaction) {
   ASSERT_EQ((size_t)4, result.size());
   const auto& hex = *std::next(result.begin());
   const auto& complete = *result.rbegin();
-  ASSERT_STREQ("0200000001f393847c97508f24b772281deea475cd3e0f719f321794e5da7cf8587e28ccb4010000009200483045022100aa60e88fd655415eea4a10947f84e844127dda8433e38dec2736bab906897b41022027ab233c232c4cb0258204e0ae6e9200da0b60319cb99e0029a6159fbd889d0d0147512103debedc17b3df2badbcdd86d5feb4562b86fe182e5998abd8bcd4f122c6155b1b21027e940bb73ab8732bfdf7f9216ecefca5b94d6df834e77e108f68e66f126044c052aeffffffff0100ab90410000000017a914b10c9df5f7edf436c697f02f1efdba4cf39961518700000000",
+  ASSERT_STREQ("0100000001f393847c97508f24b772281deea475cd3e0f719f321794e5da7cf8587e28ccb401000000910047304402206047ab554a70b921b883a56ad78859a61a9cd99da3c5efbdc1dd81909e3405330220610c720522a15cb7afe00210cca0ba1c7c88d01d42ff51602593c3985feef7cc0147512103debedc17b3df2badbcdd86d5feb4562b86fe182e5998abd8bcd4f122c6155b1b21027e940bb73ab8732bfdf7f9216ecefca5b94d6df834e77e108f68e66f126044c052aeffffffff0100ab9041000000003c76a914ef1caef64ee1e2aff877b7e8e9713dc25b2289f288ac200206260143838b5ff52dc2eb7b4b8099d4e4c99dc3ef19794289a2cd4c10070000b400000000",
             hex.c_str());
   ASSERT_STREQ("true", complete.c_str());
 }
-*/
 
 
 GTEST_API_ int main(int argc, char **argv) {
